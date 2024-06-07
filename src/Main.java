58b5f84fbd9b0e2b248b37b872681e8d34ca4b9d
Olga Odintsova <olga.a.odintsova@gmail.com>
2024-06-07 21:32:11 +0200

    Initial homework commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        //Task 1
+        System.out.println("Task 1");
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        //Task 2
+        System.out.println("\nTask 2");
+
+        String fullNameUpperCase = fullName.toUpperCase();
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpperCase);
+
+        //Task 3
+        System.out.println("\nTask 3");
+
+        fullName = "Иванов Семён Семёнович";
+        String fullNameReplacedChar = fullName.replace('ё', 'е');
+
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameReplacedChar + "\n");
     }
 }
